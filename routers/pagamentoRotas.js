4062f4b522b0571dcf73a36b8168c2423139a399
Difarias <dffreitas.cic@uesc.br>
2024-06-20 20:20:08 -0300

    Feat: Pedido's Page is working now

--- routers/pagamentoRotas.js
+++ routers/pagamentoRotas.js
@@ -1,7 +1,22 @@
 const express = require('express');
 const router = express.Router();
 const controladorProduto = require("../controllers/produtoControlador");
-const controladorCategoria = require("../controllers/categoriaControlador"); // Importe o controlador de categoria
+const controladorCategoria = require("../controllers/categoriaControlador"); 
+const controladorTipoTransacao = require("../controllers/tipoTransacaoControlador");
+const controladorPedido = require("../controllers/pedidoControlador");
+
+router.post('/finalizar', (req, res) => {
+    const novoPedido = req.body; // Os dados do pedido devem estar no corpo da requisição
+    console.log('Dados recebidos no backend:', novoPedido);
+    controladorPedido.criar(novoPedido)
+        .then(pedidoCriado => {
+            res.status(201).json(pedidoCriado); // Retornar o pedido criado como resposta
+        })
+        .catch(error => {
+            console.error('Erro ao criar pedido:', error);
+            res.status(400).json({ message: error.message }); // Tratar erros caso ocorra algum problema na criação do pedido
+        });
+});
 
 function renderizarCarrinho(req, res, nomePagina)  {
     // Receber a lista de IDs de produtos da query
@@ -19,10 +34,11 @@ function renderizarCarrinho(req, res, nomePagina)  {
     // Fazer a busca no banco de dados
     Promise.all([
         controladorCategoria.buscar(), // Buscar categorias
-        controladorProduto.buscarPorIds(idsArray) // Buscar produtos por IDs
+        controladorProduto.buscarPorIds(idsArray), // Buscar produtos por IDs
+        controladorTipoTransacao.buscar()
     ])
-    .then(([categorias, produtos]) => {
-        res.render(nomePagina, { title: nomePagina, categorias, produtos });
+    .then(([categorias, produtos, tipoTransacoes]) => {
+        res.render(nomePagina, { title: nomePagina, categorias, produtos, tipoTransacoes});
     })
     .catch((error) => res.status(400).json({ message: error.message }));
 };
