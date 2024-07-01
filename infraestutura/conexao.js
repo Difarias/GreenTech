f8d8ae2a5627b5c30800981ca500e94373082715
Difarias <dffreitas.cic@uesc.br>
2024-07-01 20:16:09 -0300

    Fix: Details Infrastructure

--- infraestutura/conexao.js
+++ infraestutura/conexao.js
@@ -3,8 +3,8 @@ const mysql = require("mysql");
 const conexao = mysql.createConnection({
     host:"localhost",
     port:3306,
-    user:"root",
-    password:"DiSaias#2",
+    user:"seu_usuario",
+    password: "sua_senha",
     database:"greenTech"
 });
 
